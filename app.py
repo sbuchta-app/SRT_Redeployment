3e4421bfdaed11f9f27985f06eb24f758510a9d7
sbuchta-app <sebastian.b.buchta@pwc.com>
2026-02-12 12:49:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ DONOR_RISK_WEIGHT: Dict[str, float] = {
     "B1_SME_TERM": 1.30,
     "B1_MIDCORP_NONIG": 1.10,
     "B1_EM_CORP": 1.00,
-    "B1_CRE_NON_HVCRE": 1.40,
+    "B1_CRE_NON_HVCRE": 1.45,
 }
 
 RECEIVER_RISK_WEIGHT: Dict[str, float] = {
